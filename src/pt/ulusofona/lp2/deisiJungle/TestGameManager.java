1ace41d8407a44b2f65820fcafae08ee0d11f710
Rafael-Silva-a22107578 <rafael.lourenco.silva@gmail.com>
2022-11-14 02:32:55 +0000

    Adicionado os testes

--- src/pt/ulusofona/lp2/deisiJungle/TestGameManager.java
+++ src/pt/ulusofona/lp2/deisiJungle/TestGameManager.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-import static org.testng.AssertJUnit.assertEquals;
+import static org.junit.Assert.assertEquals;
 
 public class TestGameManager {
 
